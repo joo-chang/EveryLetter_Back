7ce7ddc28f7e9422808d25f6fe8dbb89240dd7e7
joo-chang <jooc0311@gmail.com>
2023-10-22 13:53:08 +0900

    Add : Swagger 환경 설정 추가

---
+++ src/main/java/com/joo/everyletter_back/common/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.joo.everyletter_back.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String API_NAME = "EveryLetter API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "EveryLetter API 명세서 입니다.";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .build();
+    }
+}
