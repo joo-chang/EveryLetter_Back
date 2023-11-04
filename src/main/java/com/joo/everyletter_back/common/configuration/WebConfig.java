3ff2b6e9eabf7f70c22bf84bb8a3fde542009719
joo-chang <jooc0311@gmail.com>
2023-11-04 10:38:16 +0900

    Fix : CORS 해결

--- src/main/java/com/joo/everyletter_back/common/configuration/WebConfig.java
+++ src/main/java/com/joo/everyletter_back/common/configuration/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
 	 */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+		registry.addMapping("/**").allowCredentials(true).allowedOrigins("http://localhost:5173");
 	}
 }
