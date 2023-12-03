b0b168cb4ee9dfd076e2b576c046d20a3d053c2f
joo-chang <jooc0311@gmail.com>
2023-12-03 10:17:34 +0900

    Fix : 날짜 저장 형식 변경

--- src/main/java/com/joo/everyletter_back/common/model/entity/BaseTimeEntity.java
+++ src/main/java/com/joo/everyletter_back/common/model/entity/BaseTimeEntity.java
@@ -1,13 +1,14 @@
 package com.joo.everyletter_back.common.model.entity;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -15,11 +16,21 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity{
 
     // Entity가 생성되어 저장될 때 시간이 자동 저장됩니다.
-    @CreatedDate
-    private LocalDateTime createdDate;
+    private String createdDate;
 
     // 조회한 Entity 값을 변경할 때 시간이 자동 저장됩니다.
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    private String modifiedDate;
 
+    @PrePersist
+        //해당 엔티티 저장하기 전
+    void onPrePersist(){
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.modifiedDate = createdDate;
+    }
+
+    @PreUpdate
+        //해당 엔티티 수정 하기 전
+    void onPreUpdate(){
+        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
 }
